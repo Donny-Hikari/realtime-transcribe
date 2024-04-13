4057c54c770fe62a57b00d8827541f5b254f0f18
Donny Hikari <donny.hikari@gmail.com>
2024-04-13 17:28:24 +0900

    Merge PyAudio and SpeechRecognition sources as different input provider.

--- transcribe.py
+++ transcribe.py
@@ -8,6 +8,7 @@ import speech_recognition as sr
 import whisper
 import torch
 import threading
+import pyaudio
 
 from datetime import datetime, timedelta
 from queue import Queue
@@ -24,15 +25,19 @@ def parse_args():
             choices=["tiny", "base", "small", "medium", "large", "tiny.en", "base.en", "small.en", "medium.en", "large-v3"])
   parser.add_argument("--input", default=None,
             help="Default input source.", type=str)
+  parser.add_argument("--input-provider", default="pyaudio",
+            choices=["pyaudio", "speech-recognition"],
+            help="Default input provider.", type=str)
   parser.add_argument("--language", default=None,
             help="Default language.", type=str)
   parser.add_argument("--font-size", default=30, type=int,
             help="Font size of HUD.")
+  parser.add_argument("--no-fp16", action='store_true', default=False,
+            help="Disable fp16.")
 
-
-  parser.add_argument("--energy_threshold", default=1000,
+  parser.add_argument("--energy_threshold", default=200,
             help="Energy level for mic to detect.", type=int)
-  parser.add_argument("--record_timeout", default=1,
+  parser.add_argument("--record_timeout", default=2,
             help="How real time the recording is in seconds.", type=float)
   parser.add_argument("--phrase_timeout", default=3,
             help="How much empty space between recordings before we "
@@ -41,8 +46,8 @@ def parse_args():
   return args
 
 class HUD(QMainWindow):
-  max_width_percentage = 0.3
-  max_lines = 3
+  max_width_percentage = 0.4
+  max_lines = 4
   padding = 10
   stupid_qt = 20
 
@@ -108,69 +113,81 @@ class HUD(QMainWindow):
 
     # Set label maximum height to show only last 3 lines wrapped
     fm = self.label.fontMetrics()
-    label_rect = fm.boundingRect(QRect(0, 0, self.max_width, 1000), Qt.TextWordWrap|Qt.AlignLeft|Qt.AlignVCenter, self.text)
-    last_3_lines_height = self.max_lines * fm.lineSpacing() + fm.leading()
+    label_rect = fm.boundingRect(QRect(0, 0, self.max_width-self.padding*2, 1000), Qt.TextWordWrap|Qt.AlignLeft|Qt.AlignVCenter, self.text)
+    last_3_lines_height = self.max_lines * fm.lineSpacing()
     max_label_height = min(label_rect.height(), last_3_lines_height) + self.padding*2 + self.stupid_qt
     self.setFixedHeight(max_label_height)
     self.label.resize(QSize(label_rect.width(), label_rect.height()))
     self.label.move(0, max_label_height - label_rect.height())
 
-class Transcriber():
-  def __init__(self, args):
-    self.args = args
-    self.language = args.language
+
+class AudioInputProvider:
+  def list_input_devices(self):
+    raise NotImplementedError
+
+  def init_input_device(self, device_index):
+    raise NotImplementedError
+
+  def start_record(self):
+    raise NotImplementedError
+
+  def stop_record(self):
+    raise NotImplementedError
+
+  def is_new_phrase(self, acc_data, new_data):
+    raise NotImplementedError
+
+class SpeechRecognitionAudioProvider(AudioInputProvider):
+  def __init__(self, args, data_queue):
     self.sample_rate = 44100
-    # The last time a recording was retrieved from the queue.
-    self.compute_device = "cuda" if torch.cuda.is_available() else "cpu"
-    self.model_name = args.model
+    self.energy_threshold = args.energy_threshold
     self.record_timeout = args.record_timeout
     self.phrase_timeout = args.phrase_timeout
 
-    # Thread safe Queue for passing data from the threaded recording callback.
-    self.data_queue = Queue()
-    self.current_text = ''
-    self.transribe_thread = None
-    self.stop_event = threading.Event()
-    self.hud_window = None
+    self.data_queue = data_queue
+    self.stop_listening = None
 
+  def list_input_devices(self):
+    return sr.Microphone.list_microphone_names()
+
+  def init_input_device(self, device_index):
     # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
     self.recorder = sr.Recognizer()
-    self.recorder.energy_threshold = args.energy_threshold
+    self.recorder.energy_threshold = self.energy_threshold
     # Definitely do this, dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
     self.recorder.dynamic_energy_threshold = False
 
-    self.init_input_device(args)
-
-    # Load / Download model
-    self.audio_model = whisper.load_model(self.model_name, device=self.compute_device)
+    self.source = sr.Microphone(sample_rate=self.sample_rate, device_index=device_index)
 
     with self.source:
       self.recorder.adjust_for_ambient_noise(self.source)
 
-    # Cue the user that we're ready to go.
-    print("Model loaded.\n")
+  def start_record(self):
+    # Create a background thread that will pass us raw audio bytes.
+    # We could do this manually but SpeechRecognizer provides a nice helper.
+    self.stop_listening = self.recorder.listen_in_background(self.source, self.record_callback, phrase_time_limit=self.record_timeout)
 
-  def init_input_device(self, args):
-    device_index = None
-    if args.input is not None:
-      for idx, name in enumerate(sr.Microphone.list_microphone_names()):
-        if name == args.input:
-          device_index = idx
-          break
+    self.phrase_time = None
 
-    if device_index is None:
-      # Print the list of available audio devices
-      print("Available input devices:")
-      for idx, name in enumerate(sr.Microphone.list_microphone_names()):
-        print(f"{idx}. {name}")
+  def stop_record(self):
+    if self.stop_listening is None:
+      raise RuntimeError("Recording not started")
 
-      if args.input is None:
-        # Choose the index of the desired input device
-        device_index = int(input("Enter the index of the input device you want to use: "))
-      else:
-        raise RuntimeError(f'Invalid input device "{args.input}"')
+    self.stop_listening(True)
 
-    self.source = sr.Microphone(sample_rate=self.sample_rate, device_index=device_index)
+    del self.phrase_time
+
+  def is_new_phrase(self, acc_data, new_data):
+    now = datetime.now()
+
+    # If enough time has passed between recordings, consider the phrase complete.
+    # Clear the current working audio buffer to start over with the new data.
+    if self.phrase_time and now - self.phrase_time > timedelta(seconds=self.phrase_timeout):
+      # This is the last time we received new audio data from the queue.
+      self.phrase_time = now # should set after the transcription is done
+      return True
+    else:
+      return False
 
   def record_callback(self, _, audio: sr.AudioData) -> None:
     """
@@ -180,6 +197,112 @@ class Transcriber():
     # Grab the raw bytes and push it into the thread safe queue.
     data = audio.get_raw_data()
     self.data_queue.put(data)
+
+class PyAudioProvider(AudioInputProvider):
+  def __init__(self, args, data_queue):
+    self.audio = pyaudio.PyAudio()
+
+    self.audio_format = pyaudio.paInt16  # Format of the audio samples
+    self.audio_channels = 1  # Number of audio channels (1 for mono, 2 for stereo)
+    self.sample_rate = 44100  # Sample rate (samples per second)
+    self.sample_size = self.audio.get_sample_size(self.audio_format)
+    self.audio_chunk = 1024  # Number of frames per buffer
+
+    self.device_index = None
+    self.stop_event = threading.Event()
+
+    self.data_queue = data_queue
+
+  def list_input_devices(self):
+    for idx in range(self.audio.get_device_count()):
+      device_info = self.audio.get_device_info_by_index(idx)
+      yield device_info['name']
+
+  def init_input_device(self, device_index):
+    self.device_index = device_index
+
+  def start_record(self):
+    self.stop_event.clear()
+
+    self.record_thread = threading.Thread(target=self._record_audio)
+    self.record_thread.start()
+
+  def stop_record(self):
+    self.stop_event.set()
+    self.record_thread.join()
+
+  def is_new_phrase(self, acc_data, new_data):
+    return len(acc_data) + len(new_data) > self.sample_rate*self.sample_size*10240
+
+  def _record_audio(self):
+    # Open audio stream with the selected input device
+    stream = self.audio.open(
+      format=self.audio_format,
+      channels=self.audio_channels,
+      rate=self.sample_rate,
+      input=True,
+      input_device_index=self.device_index,
+      frames_per_buffer=self.audio_chunk,
+    )
+
+    while not self.stop_event.is_set():
+      data = stream.read(self.audio_chunk, exception_on_overflow=False)
+      self.data_queue.put(data)
+
+    # Close the audio stream
+    stream.stop_stream()
+    stream.close()
+
+class Transcriber():
+  def __init__(self, args):
+    self.args = args
+    self.language = args.language
+    self.sample_rate = 44100
+    # The last time a recording was retrieved from the queue.
+    self.compute_device = "cuda" if torch.cuda.is_available() else "cpu"
+    self.model_name = args.model
+
+    # Thread safe Queue for passing data from the threaded recording callback.
+    self.data_queue = Queue()
+    self.transribe_thread = None
+    self.stop_event = threading.Event()
+    self.hud_window = None
+
+    if args.input_provider == "speech-recognition":
+      self.input_provider = SpeechRecognitionAudioProvider(args=self.args, data_queue=self.data_queue)
+    elif args.input_provider == "pyaudio":
+      self.input_provider = PyAudioProvider(args=self.args, data_queue=self.data_queue)
+
+    self.init_input_device(args)
+
+    # Load / Download model
+    self.audio_model = whisper.load_model(self.model_name, device=self.compute_device)
+
+    # Cue the user that we're ready to go.
+    print("Model loaded.\n")
+
+  def init_input_device(self, args):
+    device_index = None
+
+    if args.input is not None:
+      for idx, name in enumerate(self.input_provider.list_input_devices()):
+        if name == args.input:
+          device_index = idx
+          break
+
+    if device_index is None:
+      # Print the list of available audio devices
+      print("Available input devices:")
+      for idx, name in enumerate(self.input_provider.list_input_devices()):
+        print(f"{idx}. {name}")
+
+      if args.input is None:
+        # Choose the index of the desired input device
+        device_index = int(input("Enter the index of the input device you want to use: "))
+      else:
+        raise RuntimeError(f'Invalid input device "{args.input}"')
+
+    self.input_provider.init_input_device(device_index)
 
   def display_hud(self):
     app = QApplication([])
@@ -203,24 +326,12 @@ class Transcriber():
 
   def listen(self):
     transcription = ['']
-    phrase_time = None
 
-    # Create a background thread that will pass us raw audio bytes.
-    # We could do this manually but SpeechRecognizer provides a nice helper.
-    self.recorder.listen_in_background(self.source, self.record_callback, phrase_time_limit=self.record_timeout)
+    self.input_provider.start_record()
 
     acc_audio_data = torch.zeros((0,), dtype=torch.float32, device=self.compute_device)
     while not self.stop_event.is_set():
       try:
-        now = datetime.now()
-        # Pull raw recorded audio from the queue.
-        phrase_complete = False
-        # If enough time has passed between recordings, consider the phrase complete.
-        # Clear the current working audio buffer to start over with the new data.
-        if phrase_time and now - phrase_time > timedelta(seconds=self.phrase_timeout):
-          phrase_complete = True
-          acc_audio_data = torch.zeros((0,), dtype=torch.float32, device=self.compute_device)
-
         self.data_queue.mutex.acquire()
         audio_data = b''.join(self.data_queue.queue)
         self.data_queue.queue.clear()
@@ -229,6 +340,10 @@ class Transcriber():
         if len(audio_data) == 0:
           sleep(0.1)
           continue
+
+        phrase_complete = self.input_provider.is_new_phrase(acc_audio_data, audio_data)
+        if phrase_complete:
+          acc_audio_data = torch.zeros((0,), dtype=torch.float32, device=self.compute_device)
 
         # Convert in-ram buffer to something the model can use directly without needing a temp file.
         # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
@@ -241,7 +356,7 @@ class Transcriber():
         # Read the transcription.
         result = self.audio_model.transcribe(
           acc_audio_data,
-          fp16=(self.compute_device == "cuda"),
+          fp16=not self.args.no_fp16 and (self.compute_device == "cuda"),
           condition_on_previous_text=False,
           language=self.language,
         )
@@ -254,7 +369,6 @@ class Transcriber():
         else:
           transcription[-1] = text
 
-        self.current_text = text
         self.hud_window.text = '\n'.join(transcription[-2:])
 
         # Clear the console to reprint the updated transcription.
@@ -262,23 +376,21 @@ class Transcriber():
         for seg in result['segments']:
           print(seg['start'], '->', seg['end'], seg['text'])
         # print(result)
-        print(len(acc_audio_data))
-        print(len(acc_audio_data)/self.source.SAMPLE_RATE/self.source.SAMPLE_WIDTH)
+        # print(len(acc_audio_data))
+        # print(len(acc_audio_data)/self.source.SAMPLE_RATE/self.source.SAMPLE_WIDTH)
 
         for line in transcription:
           print(line)
         # Flush stdout.
         print('', end='', flush=True)
-
-        # This is the last time we received new audio data from the queue.
-        phrase_time = now
       except KeyboardInterrupt as e:
-        print(e)
         break
 
     print("\n\nTranscription:")
     for line in transcription:
       print(line)
+
+    self.input_provider.stop_record()
 
     return transcription
 
